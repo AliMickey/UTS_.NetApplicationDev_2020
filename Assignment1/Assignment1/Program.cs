5df095ce2e5bc8637267a6fba941ec5e34eee257
AliMicky <ad312@pm.me>
2020-09-17 00:18:41 +1000

    add test account

--- Assignment1/Assignment1/Program.cs
+++ Assignment1/Assignment1/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
-using System.IO;
-
-namespace Assignment1
+﻿namespace Assignment1
 {
     class Program
     {
@@ -11,6 +7,6 @@ namespace Assignment1
             // https://github.com/AliMickey/UTS_.NetApplicationDev_Spring_2020/commits/master
             Bank bank = new Bank();
             bank.LoginInit();
-        }      
+        }
     }
 }
